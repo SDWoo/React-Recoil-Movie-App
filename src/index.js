bb24fd657fbffecaa8b0f7ce594ebfaa54a2521a
sdwoo <damjang67@naver.com>
2023-01-04 13:46:35 +0900

    refactor: 폰트 설정 및 정리

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RecoilRoot } from 'recoil';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <Suspense fallback={<div>Loading...</div>}>
+        <App />
+      </Suspense>
+    </RecoilRoot>
   </React.StrictMode>
 );
 

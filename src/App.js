69a572184d5b65adb498335b1019080a5e511754
sdwoo <damjang67@naver.com>
2023-01-04 13:45:30 +0900

    feat: Router 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import MoviePage from './pages/MoviePage';
+
 function App() {
-  return <div></div>;
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<MainPage />} />
+          <Route path='/movie/:movieId' element={<MoviePage />} />
+        </Routes>
+      </BrowserRouter>
+      ;
+    </div>
+  );
 }
 
 export default App;
